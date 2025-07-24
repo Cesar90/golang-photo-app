4f50b235e71985716b90cc3a77c2e21d5dd20ec5
Cesar <ccordero.007@gmail.com>
2025-07-24 00:01:39 -0600

    .gitignore and .air.toml setting

--- main.go
+++ main.go
@@ -16,6 +16,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at</p><a href=\"\">test</a>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+		<ul>
+			<li>
+				Is there a free version? YES! We Offer a free trial for 30 days on any
+				paid plans
+			</li>
+		</ul>
+
+	`)
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
@@ -38,6 +51,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		// w.WriteHeader(http.StatusNotFound)
 		// fmt.Fprint(w, "Page not found")
