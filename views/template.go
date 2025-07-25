b86cfff98a43caad98fe7e8b0cff036de127f4d7
Cesar <ccordero.007@gmail.com>
2025-07-25 11:50:06 -0600

    Made the FAQ page use dynamically provided questions and answers

--- views/template.go
+++ views/template.go
@@ -42,7 +42,7 @@ type Template struct {
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	// fmt.Fprint(w, "<h1>Welome to my awesome site!!</h1>")
-	err := t.htmlTpl.Execute(w, nil)
+	err := t.htmlTpl.Execute(w, data)
 	if err != nil {
 		// panic(err) //TODO: Remove the panic
 		log.Printf("parsing template %v", err)
