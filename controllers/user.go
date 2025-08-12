d72b49275d21a8e506a808940c4f20ebf1e64be7
Cesar <ccordero.007@gmail.com>
2025-08-12 00:28:43 -0600

    Sign Out Handler and user Controller

--- controllers/user.go
+++ controllers/user.go
@@ -148,3 +148,20 @@ func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Email cookie: %s \n", email.Value)
 	// fmt.Fprintf(w, "Headers: %+V \n", r.Header)
 }
+
+func (u Users) ProcessSignOut(w http.ResponseWriter, r *http.Request) {
+	token, err := readCookie(r, "CookieSession")
+	if err != nil {
+		http.Redirect(w, r, "/signin", http.StatusFound)
+		return
+	}
+	err = u.SessionService.Delete(token)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	deleteCookie(w, "CookieSession")
+	http.Redirect(w, r, "/signin", http.StatusFound)
+}
