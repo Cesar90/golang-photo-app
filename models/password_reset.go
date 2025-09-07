d3373190903fbdf68d573aae9aeacc41c4e15774
Cesar <ccordero.007@gmail.com>
2025-09-07 15:55:17 -0600

    PasswordReset Creating

--- models/password_reset.go
+++ models/password_reset.go
@@ -1,9 +1,14 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/Cesar90/golang-photo-app/rand"
 )
 
 const (
@@ -29,12 +34,67 @@ type PasswordResetSevice struct {
 	// Duration is the amount of time that a PasswordReset is valid for.
 	// Defaults to DefaultResetDuration
 	Duration time.Duration
+
+	// This can be an option but it will become complex in testing
+	// Now func() time.Time
 }
 
 func (service *PasswordResetSevice) Create(email string) (*PasswordReset, error) {
-	return nil, fmt.Errorf("TODO: Implement PasswordResetService.Create")
+	// Verify we have a valid email address for a user, and get that user's ID
+	email = strings.ToLower(email)
+	var userID int
+	row := service.DB.QueryRow(`
+		SELECT id FROM users WHERE email = $1`, email)
+	err := row.Scan(&userID)
+	if err != nil {
+		// TODO: Consider return a specific error when the user does not exist.
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	// Build the PasswordReset
+	bytesPerToken := service.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("craete: %w", err)
+	}
+
+	duration := service.Duration
+	if duration == 0 {
+		duration = DefaultResetDuration
+	}
+
+	pwReset := PasswordReset{
+		UserID:    userID,
+		Token:     token,
+		TokenHash: service.hash(token),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	// Insert the PasswordReset into the DB
+	row = service.DB.QueryRow(`
+		INSERT INTO password_resets (user_id, token_hash, expires_at)
+		VALUES ($1, $2, $3) ON CONFLICT (user_id) DO 
+		UPDATE 
+		SET token_hash = $2, expires_at = $3
+		RETURNING id;`, pwReset.UserID, pwReset.TokenHash, pwReset.ExpiresAt)
+
+	err = row.Scan(&pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	// return nil, fmt.Errorf("TODO: Implement PasswordResetService.Create")
+	return &pwReset, nil
 }
 
 func (service *PasswordResetSevice) Consume(token string) (*User, error) {
 	return nil, fmt.Errorf("TODO: Implement PasswordResetService.Consume")
+}
+
+func (service *PasswordResetSevice) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
