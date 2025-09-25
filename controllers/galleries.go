131d737855da363f06c2c084eaa1169dca1ca52b
Cesar <ccordero.007@gmail.com>
2025-09-25 11:56:04 -0600

    New Gallery Template

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -17,7 +17,8 @@ func (g Galleries) New(w http.ResponseWriter, r *http.Request) {
 	var data struct {
 		Title string
 	}
-
+	//Read title variable from url
+	//http://localhost:3000/signup?title=Many%20Many%20cats
 	data.Title = r.FormValue("title")
 	g.Templates.New.Execute(w, r, data)
 }
